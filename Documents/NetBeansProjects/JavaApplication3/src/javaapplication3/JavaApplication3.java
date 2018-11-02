607302b8f8b6b4d490f31c4dbb3d9282de2a9e8b
breakdesmond <desmondwch@yahoo.com>
2018-11-02 10:50:49 +0800

    Revert "Practical 2 Agile 2.0"
    This reverts commit 8a12b3262a3e9cc9b89b6fbf88497df43b989a80.

--- Documents/NetBeansProjects/JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ Documents/NetBeansProjects/JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -15,7 +15,6 @@ public class JavaApplication3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hi");
         // TODO code application logic here
     }
     
